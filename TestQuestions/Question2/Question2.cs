516e245a660792a1cb388b6b6dea4ce08faaf853
Jando3 <JSand03330@gmail.com>
2023-04-01 11:17:34 -0400

    Add project files.

---
+++ TestQuestions/Question2/Question2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CodingTest.TestQuestions.Question2
+{
+    public class MatchingAlgorithm
+    {
+        /// <summary>
+        /// A Customer is looking to match their contact list to their users.  We have a list of contacts and a listing of their
+        /// active directory logins for each computer.  Unfortunately, over the years, they have had different models of how they create
+        /// their active directory logins, and so our algorithm should be able to determine whether a username matches based on a few different
+        /// patterns
+        /// </summary>
+        /// <param name="FirstName"></param>
+        /// <param name="LastName"></param>
+        /// <param name="activeDirectoryLogin"></param>
+        /// <returns></returns>
+        public bool IsMatch(string FirstName, string LastName, string activeDirectoryLogin)
+        {
+            throw new NotImplementedException();
+            public class firstAlgo
+        {
+            var split = activeDirectoryLogin.Split('\\').last();
+            
+            //non active domain case handle
+            if(split == null){ split = activeDirectoryLogin;
+
+            //first initial & last name
+            if(split.StartsWith(FirstName[0], StringComparison.OrdinalIgnoreCase) && split.Substring(1).Equals(LastName, StringComparison.OrdinalIgnoreCase)) {return true;}
+
+            //firstname.lastname
+            if (split.Equals($'{FirstName.ToLower() }.{LastName.ToLower() }', StringComparison.OridnalIgnoreCase)){return true;}       
+ 
+            //FirstName.MiddleInitial.LastName ... this one confused me bcause the question doest say to assume theres a  middle initial.
+            if (split.Equals($'{FirstName.ToLower()}.{MiddleInitial.ToLower()}.{LastName.ToLower()}', StringComparison.OrdinalIgnoreCase)){return true;}
+
+                return false;
+        }
+        }
+    public class Question
+    } 
+}
---
+++ TestQuestions/Question3/Question3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Sockets;
+using CodingTest.TestQuestions.Question3.Application;
+using CodingTest.TestQuestions.Question3.Models;
+using CodingTest.TestQuestions.Question3.Repositories;
+
+namespace CodingTest.TestQuestions.Question3
+{
+    public class SiteCreationService : ISiteCreationService
+    {
+        private readonly ISiteRepository _repository;
+        private readonly ITaxesRepository _taxesRepository;
+
+        public SiteCreationService(ISiteRepository repository, ITaxesRepository taxesRepository)
+        {
+            _repository = repository;
+            _taxesRepository = taxesRepository;
+        }
+
+        /// <summary>
+        /// This function takes in the SiteViewModel from our application and looks up the zip code to find the applicable tax rates, then
+        /// creates the site with the tax rates added.  If the tax rate cannot be found, throw an InvalidOperationException; if the
+        /// site already exists, throw an InvalidOperationException.  Save the site to the database, and return the newly created site.
+        /// </summary>
+        /// <param name="svm"></param>
+        public SiteEntity CreateSite(SiteViewModel svm)
+        {
+
+            //Site exists?
+            if (_repository.SiteExists(svm.SiteName))
+            { throw new InvalidOperationException('Site Already Exists.');
+            }
+            //Get Tax Rates by zip
+            var taxRates = _taxesRepository.GetTaxRatesByZip(svm.ZipCode);
+            if (taxRates == null)
+            { throw new InvalidOperationException('Tax Rate not found')}
+
+            //Calculates total tax rate
+            var totalTaxRate = taxRates.Sum(x => x.Rate);
+            //create site
+            var site = new SiteEntity
+            { siteName = svm.SiteName,
+                AddressFamily = new Address {
+                    City = svm.City,
+                    State = svm.State,
+                    ZipCode = svm.ZipCode
+                },
+                TaxRate = totalTaxRate
+            };
+            //save site
+            _repository.CreateSite(site);
+
+            return site;
+
+        }
+    }
+}
